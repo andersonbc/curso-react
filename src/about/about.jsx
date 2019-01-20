98a63ebd8b1fce43755c41a98fdde9ea990e7060
Anderson Botelho <andersonbcdev@gmail.com>
2019-01-20 17:23:31 -0200

    modulo 7 completo curso react

---
+++ src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='sobre' small='nós'></PageHeader>
+
+    <h2>Nossa história</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus reiciendis 
+      recusandae rem perspiciatis incidunt autem maiores eos! Aut quis sint incidunt ut repellendus ad facere ducimus atque. Deleniti, facilis ipsum.</p>
+  </div>
+)
